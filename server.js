aaf9f40c64f7bc9046f2fb8c2195f710215adbbd
MD ABRAR KHAN <70259069+akhan172@users.noreply.github.com>
2025-10-22 21:38:15 +0530

    Set up Express server with a simple route

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from simple-app!'));
+app.listen(PORT, () => console.log('Listening on', PORT));
 
